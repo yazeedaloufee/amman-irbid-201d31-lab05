c1e14562831e094a2608dbaf6aa6e5bc74d4c01f
YAZEED ALOUFEE <YAZEEDALOUFEE@GMAIL.COM>
2021-06-24 14:00:55 +0300

    solved problem 3

--- app.js
+++ app.js
@@ -53,11 +53,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
+  let num1 =a;
+  let num2 =b;
+  let num3 =c;
+  let sum1 =sum(num1,num2)[0]
+  let sum2 =sum(sum1,num3)[0]
+  let multi1 =multiply(num1,num2)[0]
+  let multi2 =multiply(multi1,num3)[0]
+  return[sum2, multi2,`${num1} and ${num2} and ${num3} sum to ${sum2}.`,`The product of ${num1} and ${num2} and ${num3} is ${multi2}.`]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
